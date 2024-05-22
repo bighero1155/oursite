b7493493d38e759779fd8bf2887a301d63da5b1a
bighero1155 <alexvincentvbravo@gmail.com>
2024-05-22 10:11:34 +0800

    Added insert and read gender feature

--- djangocrudlesson/urls.py
+++ djangocrudlesson/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('genders', views.index_gender),
+    path('gender/create', views.create_gender),
+    path('gender_store', views.store_gender),
 ]
--- djangocrudlesson/views.py
+++ djangocrudlesson/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Gender
+from django.contrib import messages
 
 # Create your views here.
 def index_gender(request):
-    return render(request, 'gender/index.html')
+    genders = Gender.objects.all() #select * from genders
+
+    context = {
+        'genders': genders
+    }
+
+    return render(request, 'gender/index.html', context)
+
+
+def create_gender(request):
+    return render(request, 'gender/create.html')
+
+def store_gender(request):
+    gender = request.POST.get('gender')
+    Gender.objects.create(gender=gender) #insert into genders(gender) values(gender)
+    messages.success(request, 'Gender successfully saved!')
+    return redirect('/genders')
